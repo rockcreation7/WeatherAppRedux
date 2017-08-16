1ef78567a574c32089a245774c540b37eae62b5a
rockcreation7 <rockcreation7@gmail.com>
2017-08-16 23:46:17 +0800

    Add unknow comment
    Add unknow comment

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -22,8 +22,14 @@ class WeatherList extends Component{
     }
 }
 
-function mapStateToProps({weather}) {
+function mapStateToProps({ weather }){
+ // this get the weather from state
  return { weather };
+ // why? is it set up a state here? Just like constructor in the past?
+ // it will connect to redcuer/redux
 }
 
+// it will use mapStateToProps function and pass state
+// but how can it can pass state without paramater?
 export default connect(mapStateToProps, null)(WeatherList);
+// As man said it is new systax, will pass variable into the function inside function
